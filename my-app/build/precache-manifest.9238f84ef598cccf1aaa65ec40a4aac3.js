a8bed7e0a464faa3bf12878f18cc7ac7344f69f3
rohan9025 <nrgoun@gmail.com>
2020-06-11 18:26:14 +0530

    build

---
+++ my-app/build/precache-manifest.9238f84ef598cccf1aaa65ec40a4aac3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b327ab5938c4ca212a0af8ec96bd16",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "acef33989d17024df051",
+    "url": "/my-app/static/css/main.5a02a428.chunk.css"
+  },
+  {
+    "revision": "8030ea7ca966b604966f",
+    "url": "/my-app/static/js/2.a4b4a6ef.chunk.js"
+  },
+  {
+    "revision": "37c9cc0c7392e39a855781f51bc0367a",
+    "url": "/my-app/static/js/2.a4b4a6ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acef33989d17024df051",
+    "url": "/my-app/static/js/main.e354e03e.chunk.js"
+  },
+  {
+    "revision": "660024b2ea62aa0a0b7b",
+    "url": "/my-app/static/js/runtime-main.5119dbb0.js"
+  },
+  {
+    "revision": "7b411b6cc50e5b63a18632aadca1dfab",
+    "url": "/my-app/static/media/bg.7b411b6c.png"
+  },
+  {
+    "revision": "df2ea8d2be48c42250c99a128af9e4c4",
+    "url": "/my-app/static/media/gitdp.df2ea8d2.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "816d946f79c4abb32edd13935e14376f",
+    "url": "/my-app/static/media/retro.816d946f.jpeg"
+  }
+]);
